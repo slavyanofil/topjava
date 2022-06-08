1185d7fea68e7ae72c563236a2aff29e49bf2e12
Vyacheslav Trufakin <slavyanofil@gmail.com>
2022-06-08 14:35:41 +0400

    HW01 optional prepare

--- src/main/java/ru/javawebinar/topjava/storage/AbstractStorage.java
+++ src/main/java/ru/javawebinar/topjava/storage/AbstractStorage.java
@@ -1,4 +1,35 @@
 package ru.javawebinar.topjava.storage;
 
-public class AbstractStorage {
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class AbstractStorage implements Storage {
+    private static final Logger log = getLogger(AbstractStorage.class);
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void save(Meal meal) {
+        log.debug("save " + meal);
+    }
+
+    @Override
+    public Meal get(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(int id) {
+
+    }
 }
