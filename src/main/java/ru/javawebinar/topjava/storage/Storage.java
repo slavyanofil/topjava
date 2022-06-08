af3441ddd6fa7d7cad56c3814be35f5c3cd28b55
Vyacheslav Trufakin <slavyanofil@gmail.com>
2022-06-08 14:35:03 +0400

    HW01 optional prepare

--- src/main/java/ru/javawebinar/topjava/storage/Storage.java
+++ src/main/java/ru/javawebinar/topjava/storage/Storage.java
@@ -1,4 +1,15 @@
 package ru.javawebinar.topjava.storage;
 
+import ru.javawebinar.topjava.model.Meal;
+
 public interface Storage {
+    void clear();
+
+    void save(Meal meal);
+
+    Meal get(int id);
+
+    void delete(int id);
+
+    void update(int id);
 }
